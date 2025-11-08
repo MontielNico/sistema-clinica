ed85351401f20f740b320741cc0826ced2822528
Nicolas Montiel <montielnicolas848@gmail.com>
2025-11-08 13:43:54 -0300

    ajustes en dashboard medicos

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -17,6 +17,8 @@ const badgeVariants = cva(
           'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
           'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+        disabled:
+          'bg-white-300 text-gray-600 border border-gray-150 cursor-not-allowed',
       },
     },
     defaultVariants: {
